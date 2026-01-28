354be922220dd0ed58aa0214a638fe143d65202b
Diego G <diegogamero004@gmail.com>
2026-01-28 17:03:16 -0500

    Initial commit: Project setup/frontend skeleton

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,22 @@
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+    return (
+        <Tabs screenOptions={{ tabBarActiveTintColor: "#6320c7" }}>
+            <Tabs.Screen 
+             name="index" 
+              options={{
+                title: "Status",
+                headerTitle: "Packed",
+                headerTintColor: "#6320c7", 
+                }} />
+            <Tabs.Screen name="submit"
+             options={{ 
+                title: "Submit",
+                headerTitle: "Submit Location Status",
+                headerTintColor: "#6320c7",
+                }}
+            />
+        </Tabs>
+    )
+}
