2e39fe692d711aad7fa5cfe2dac4b0a56c6e3ff5
josi0621 <joselynchavez0621@gmail.com>
2026-02-20 01:51:26 -0500

    Upgraded the location UI for GPS tracking
    When you click on location, the new location UI will appear to ask you if you're nearby or if you want to return to home.

--- src/config/studySpots.tsx
+++ src/config/studySpots.tsx
@@ -4,30 +4,40 @@ interface Spot {
     id: string;
     displayName: string;
     image: ImageSourcePropType;
+    lat: number;
+    lng: number;
+    radius: number;
 }
 export const spots: Spot[] = [
-{ id: "ksl", displayName: "KSL",
-image: require("../../assets/ksl.png"),
-lat: 41.507386829405924,
-lng: -81.60966697850489,
-radius: 30, },
-{ id: "tink",
-displayName: "Tink",
-image: require("../../assets/tink.png"),
-lat: 41.50811997396701,
-lng: -81.60880062499287,
-radius: 50, },
-{ id: "pbl",
-displayName: "PBL",
-image: require("../../assets/pbl.png"),
-lat: 41.50994978625697,
-lng: -81.6080522886778,
-radius: 50, },
-{ id: "tomlinson",
-displayName: "Tomlinson",
-image: require("../../assets/tomlinson.png"),
-lat: 41.50404639714933,
-lng: -81.60959992327929,
-radius: 30,
-},
+    {
+        id: "ksl", displayName: "KSL",
+        image: require("../../assets/ksl.png"),
+        lat: 41.507386829405924,
+        lng: -81.60966697850489,
+        radius: 30,
+    },
+    {
+        id: "tink",
+        displayName: "Tink",
+        image: require("../../assets/tink.png"),
+        lat: 41.50811997396701,
+        lng: -81.60880062499287,
+        radius: 50,
+    },
+    {
+        id: "pbl",
+        displayName: "PBL",
+        image: require("../../assets/pbl.png"),
+        lat: 41.50994978625697,
+        lng: -81.6080522886778,
+        radius: 50,
+    },
+    {
+        id: "tomlinson",
+        displayName: "Tomlinson",
+        image: require("../../assets/tomlinson.png"),
+        lat: 41.50404639714933,
+        lng: -81.60959992327929,
+        radius: 30,
+    },
 ];
