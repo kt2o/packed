0ea97d8563ab413827214f5d61bbe029858a82c5
DeethyaMak <dgm92@case.edu>
2026-03-16 16:29:39 -0400

    Downgrade Clerk to 2.17.0 for Expo SDK 54

---
+++ supabase/push/index.tsx
@@ -0,0 +1,47 @@
+import { createClient } from "@supabase/supabase-js";
+
+console.log('Hello from Functions!')
+
+interface Notification {
+  id: string
+  user_id: string
+  body: string
+}
+
+interface WebhookPayload {
+  type: 'INSERT' | 'UPDATE' | 'DELETE'
+  table: string
+  record: Notification
+  schema: 'public'
+  old_record: null | Notification
+}
+
+const supabase = createClient(
+  Deno.env.get('EXPO_PUBLIC_SUPABASE_URL')!,
+  Deno.env.get('EXPO_PUBLIC_SUPABASE_ANON_KEY')!
+)
+
+Deno.serve(async (req) => {
+  const payload: WebhookPayload = await req.json()
+  const { data } = await supabase
+    .from('user_push_notifications')
+    .select('expo_push_token')
+    .eq('id', payload.record.user_id)
+    .single()
+
+  const res = await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      to: data?.expo_push_token,
+      sound: 'default',
+      body: payload.record.body,
+    }),
+  }).then((res) => res.json())
+
+  return new Response(JSON.stringify(res), {
+    headers: { 'Content-Type': 'application/json' },
+  })
+})
